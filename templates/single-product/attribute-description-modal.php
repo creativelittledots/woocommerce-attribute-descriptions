5c24dbda2e36c27bb4f71b2c3ff2fc7bb88de666
terence1990 <terence@creativelittledots.co.uk>
2015-08-20 10:21:51 +0100

    Css Changes
    Css Changes

--- templates/single-product/attribute-description-modal.php
+++ templates/single-product/attribute-description-modal.php
@@ -3,6 +3,8 @@
 	<div class="message-container">
 		
 		<a href="#" class="woocommerce-attribute-close" data-description="<?php echo $attribute_description; ?>">X</a>
+		
+		<h4><?php _e('Additional Information', 'woocommerce-attribute-descriptions'); ?></h4>
 	
 		<p class="message"></p>
 		
